5cab8595be5e85eb934d45565ea0996dd69e0edc
Hamdate <elhadjibrahim729@gmail.com>
2026-07-28 14:27:29 +0000

    Affichage message

---
+++ src/fana/Classe1.java
@@ -0,0 +1,15 @@
+package fana;
+
+public class Classe1 {
+
+	public Classe1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello")
+
+	}
+
+}
